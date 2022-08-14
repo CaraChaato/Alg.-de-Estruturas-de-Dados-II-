ab2750be0812c3e194cf6c5dbcbffab5db3cbe9f
João Victor <97927354+jvictorpdl@users.noreply.github.com>
2022-08-14 10:44:07 -0300

    Create Q5 jv.cpp

---
+++ Q5 jv.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int fib(){
+    int n[10],i=0,cont;
+    int contador = 1, num1=0, num2=1, proxNum;
+
+    printf("Sequencia de Fibonacci %d %d ", num1, num2);
+    for (i=0; i < 8; i++){
+        proxNum = num1 + num2;
+        num1 = num2;
+        num2 = proxNum;
+        contador++;
+        printf("%d ", proxNum);
+    }
+    
+    printf("\nContador = %d", contador);
+    
+    return 0;
+}
+
+
+int main(){
+    //!showMemory(start=6562)
+    int i;
+    
+    
+    fib();
+}
