7c975a0c44f54ac2c13cde7b03e417a345dca322
João Victor <97927354+jvictorpdl@users.noreply.github.com>
2022-08-14 16:10:02 -0300

    Update Q5 jv.cpp

--- Q5 jv.cpp
+++ Q5 jv.cpp
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int fib(){
-    int n[10],i=0,cont;
+    int i=0;
     int contador = 1, num1=0, num2=1, proxNum;
 
-    printf("Sequencia de Fibonacci %d %d ", num1, num2);
+    printf("Sequencia de Fibonacci: %d %d ", num1, num2);
     for (i=0; i < 8; i++){
         proxNum = num1 + num2;
         num1 = num2;
