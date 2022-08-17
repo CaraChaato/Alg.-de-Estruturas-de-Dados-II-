9366c05f9a4c6acb467f87bb1d3a7978858a2fa8
João Victor <97927354+jvictorpdl@users.noreply.github.com>
2022-08-17 09:53:16 -0300

    Update Q5 jv.cpp

--- Q5 jv.cpp
+++ Q5 jv.cpp
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
+//funcao fibonacci
 int fib(){
     int i=0;
     int contador = 1, num1=0, num2=1, proxNum;
 
     printf("Sequencia de Fibonacci: %d %d ", num1, num2);
+    
+    //laço para fazer o calculo de fibonacci e adicionar um ao contador a cada repetição.
     for (i=0; i < 8; i++){
         proxNum = num1 + num2;
         num1 = num2;
@@ -23,6 +26,6 @@ int main(){
     //!showMemory(start=6562)
     int i;
     
-    
+    //chama a funcao de fibonacci
     fib();
 }
