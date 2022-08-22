848c23c75c490326b45e69a0d4587023190784c3
João Victor <97927354+jvictorpdl@users.noreply.github.com>
2022-08-22 17:19:32 -0300

    Update Q5 jv.cpp

--- Q5 jv.cpp
+++ Q5 jv.cpp
@@ -1,31 +1,38 @@
+
 #include <stdio.h>
 
-//funcao fibonacci
-int fib(){
-    int i=0;
-    int contador = 1, num1=0, num2=1, proxNum;
+int vet[51];
 
-    printf("Sequencia de Fibonacci: %d %d ", num1, num2);
-    
-    //laço para fazer o calculo de fibonacci e adicionar um ao contador a cada repetição.
-    for (i=0; i < 8; i++){
-        proxNum = num1 + num2;
-        num1 = num2;
-        num2 = proxNum;
-        contador++;
-        printf("%d ", proxNum);
+//funcao para calcular fibonacci e restornar o valor ja calculado se ele estiver na memoria sem precisao de mais um calculo.
+int fib(int n){
+    if(n < 2){
+        return n;
+    }else if(vet[n] != -10){
+        return vet[n];
+    }else{
+        
+        vet[n] = fib(n-1) + fib(n-2);
+        return vet[n];
     }
-    
-    printf("\nContador = %d", contador);
-    
-    return 0;
 }
 
 
 int main(){
-    //!showMemory(start=6562)
-    int i;
-    
-    //chama a funcao de fibonacci
-    fib();
+    //!showMemory(start=65520)
+    int n, res, k;
+
+    //dar o valor -10 para todos os componentes do vetor, utilizando isso dentro da funcao fib para identificar se o fibonacci do numero ja foi calculado ou nao.
+for (int i=0; i<51; i++){
+    vet[i] = -10;
 }
+    
+    //recebe quantas vezes vai ser calculado o fib
+    printf("Digite o valor de N:");
+    scanf("%d", &n);
+    
+    res = fib(n);
+    printf("fib(%d) = %d \n" , n , res);
+    return 0;
+}
+
+
