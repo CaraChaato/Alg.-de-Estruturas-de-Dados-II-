8448ed0f3dcb521d9da5975d999641c418a410f4
Wesley Monteiro de Araujo <90140946+WesleyMAraujo@users.noreply.github.com>
2022-09-01 07:22:14 -0300

    Update Q07.cpp
    adição da função sqrt no resultado para comparar

--- Q07.cpp
+++ Q07.cpp
@@ -4,14 +4,14 @@
 float aproximacao(float x, float x0,float e){
     float raiz; //variavel raiz da funcao aproximacao
     if (pow(x0,2)- x <= e ) { //estrutura de decissao
-        raiz = (x0 * x0 + x) / (2 * x0); //calculo para encontrar a aproximação da raiz
+        raiz = (pow(x0,2) + x) / (2 * x0); //calculo para encontrar a aproximação da raiz
     }
     return raiz; //retorna o valor aproximado da raiz
 }
 
 int main (){
-    float x = 13, x0 = 3.5, e = 0.001, raiz; //variaveis com os valores para passar como paramatro
+    float x = 13, x0 = 3.5, e = 0.001, raiz, aux = sqrt(x); //variaveis com os valores para passar como paramatro
     raiz = aproximacao(x, x0, e); //recebe o retorno e atribui a variavel raiz da funcao main
-    printf("resultado da aproximacao: %.3f\n",raiz); //imprime o resultado da funcao aproximacao
+    printf("resultado da aproximacao: %.3f\nresultado da raiz com sqrt: %.3f",raiz, aux); //imprime o resultado da funcao aproximacao
     return 0;
 }
